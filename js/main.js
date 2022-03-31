bc852cf9dd65e1978eeb360ef3d1e107bc560ec8
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2022-03-31 17:49:53 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// let btn = document.getElementById( "btn" );
+// let thank = document.querySelector( ".modal" );
+// let hid = document.querySelectorAll( ".exit" );
+// btn.onclick = function () {
+  
+//   thank.style.display = "block"
+// }
+// hid.forEach(e => {
+//   e.onclick = function () {
+//   thank.style.display = "none";
+//   }
+// } );
+// function hh() {
+  
+//   thank.style.display = "block";
+// }
